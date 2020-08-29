5eb2a86633f3fd57f041a05640f074d739c67df1
gopalk <gopalk@motorola.com>
2020-08-29 22:07:16 +0530

    export app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+export { default as App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
